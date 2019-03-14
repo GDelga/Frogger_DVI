46ba0892294c6cab579514814c98f28d9dd5d758
carlosgocereceda <carlosgcereceda@hotmail.es>
2019-03-14 22:51:08 +0100

    refactor

--- entities.js
+++ entities.js
@@ -288,65 +288,6 @@ Death.prototype = new Sprite();
 Death.prototype.step = function(dt) {
 };
 
-
-/*Car.prototype.hit = function (damage) {
-  this.health -= damage;
-  if (this.health <= 0) {
-    if (this.board.remove(this)) {
-      this.board.add(new Explosion(this.x + this.w / 2,
-        this.y + this.h / 2));
-    }
-  }
-
-}*/
-/*
-var Enemy = function (blueprint, override) {
-  this.merge(this.baseParameters);
-  this.setup(blueprint.sprite, blueprint);
-  this.merge(override);
-}
-
-Enemy.prototype = new Sprite();
-Enemy.prototype.baseParameters = {
-  A: 0, B: 0, C: 0, D: 0,
-  E: 0, F: 0, G: 0, H: 0,
-  t: 0, health: 20, damage: 10
-};
-
-
-Enemy.prototype.type = OBJECT_ENEMY;
-
-Enemy.prototype.step = function (dt) {
-  this.t += dt;
-  this.vx = this.A + this.B * Math.sin(this.C * this.t + this.D);
-  this.vy = this.E + this.F * Math.sin(this.G * this.t + this.H);
-  this.x += this.vx * dt;
-  this.y += this.vy * dt;
-  if (this.y > Game.height ||
-    this.x < -this.w ||
-    this.x > Game.width) {
-    this.board.remove(this);
-  }
-
-  var collision = this.board.collide(this, OBJECT_PLAYER);
-  if (collision) {
-    collision.hit(this.damage);
-    this.board.remove(this);
-  }
-
-}
-
-Enemy.prototype.hit = function (damage) {
-  this.health -= damage;
-  if (this.health <= 0) {
-    if (this.board.remove(this)) {
-      this.board.add(new Explosion(this.x + this.w / 2,
-        this.y + this.h / 2));
-    }
-  }
-
-}
-*/
 //BACKGROUND
 var BackGround = function () {
 
